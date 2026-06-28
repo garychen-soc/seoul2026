5bb2e6d4f76afcbbff54229ccf60ac379c9962ad
garychen-soc <64972768+garychen-soc@users.noreply.github.com>
2026-06-29 02:37:59 +0800

    Fix KKday search links

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'seoul2026-v4';
+const CACHE_NAME = 'seoul2026-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
